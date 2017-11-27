b75d3c16a240f3b7a86b6d4fb84c7d08fd747aab
Weeken Gao <weeken@koodar.com>
2017-11-27 14:26:21 +0800

    加入webpack打包

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,11 @@
-import VueWangeditor from './src/editor.vue'
+import VueWangeditor from './editor.vue'
 
 /* istanbul ignore next */
 VueWangeditor.install = function (Vue) {
   Vue.component(VueWangeditor.name, VueWangeditor)
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(VueWangeditor)
 }
 
 export default VueWangeditor
